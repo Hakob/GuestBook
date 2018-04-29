257031119271f87d6ca99f19ed469a82fd745cf3
Hakob <hakoaraqelyan@gmail.com>
2018-04-29 14:24:55 +0400

    Advancing views with generic ones

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('sign/', views.sign, name='sign')
+    path('', views.HomeView.as_view(), name='index'),
+    path('sign/', views.SignView.as_view(), name='sign')
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView
+from django.contrib import messages
 
 from .models import Comment 
 from .forms import CommentForm
 
 
-def index(request):
-    comments = Comment.objects.order_by('-date_added')
+class HomeView(ListView):
+    model = Comment
+    template_name = 'guestbook/index.html'
+    ordering = ['-date_added']
 
-    context = {'comments' : comments}
+    # def get(self, request, *args, **kwargs):
+    #     comments = Comment.objects.order_by('-date_added')
 
-    return render(request, 'guestbook/index.html', context)
+    #     context = {'comments' : comments}
 
-def sign(request):
-    if request.method == 'POST':
+    #     return render(request, 'guestbook/index.html', context)
+
+
+class SignView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = CommentForm()
+        context = {'form' : form}
+        return render(request, 'guestbook/sign.html', context)
+
+    def post(self, request, *args, **kwargs):
         form = CommentForm(request.POST)
 
         if form.is_valid():
             new_comment = Comment(name=request.POST['name'], comment=request.POST['comment'])
             new_comment.save()
             return redirect('index')
-
-    else:
-        form = CommentForm()
-
-    context = {'form' : form}
-    return render(request, 'guestbook/sign.html', context)
+        else:
+            messages.add_message(request, messages.INFO, 'Form is not valid')
+            redirect('sign')
